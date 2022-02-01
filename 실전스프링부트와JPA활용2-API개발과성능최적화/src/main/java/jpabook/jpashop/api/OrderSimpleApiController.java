b38d3758918c9aae16f496408acc5e9c068fa042
Ea-Bell <cjp975@naver.com>
2022-02-01 20:59:08 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화
    간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화
    
    엔티티를 페치 조인(fetch join)을 사용해서 쿼리 1번에 조회
    페치 조인으로 order -> member , order -> delivery 는 이미 조회 된 상태 이므로 지연로딩X

---
+++ 실전스프링부트와JPA활용2-API개발과성능최적화/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,90 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/**
+ * X to One(ManyToOne, OneToOne)
+ * Order
+ * Order -> Memeber
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto>orderV2(){
+        //ORDER 2개
+        //N+1 -> 1+ 회원 N +  배송 N
+
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(m -> new SimpleOrderDto(m))
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders=orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(order -> new SimpleOrderDto(order))
+                .collect(Collectors.toList());
+        return result;
+
+    }
+
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 초기화
+            orderDate= order.getOrderDate();
+            orderStatus=order.getStatus();
+            address=order.getDelivery().getAddress(); //Lazy 초기화
+
+        }
+    }
+
+
+}
