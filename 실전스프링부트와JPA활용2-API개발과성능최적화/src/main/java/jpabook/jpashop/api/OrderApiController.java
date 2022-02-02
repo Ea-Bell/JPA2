885531633530fcba27d0f0613da56d194d7715c2
Ea-Bell <cjp975@naver.com>
2022-02-03 03:38:54 +0900

    API 개발 고급 - 컬렉션 조회 최적화
    주문 조회V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화
    
     * Query: 루트 1번, 컬렉션 1번
     * ToOne 관계들을 먼저 조회하고, 여기서 얻은 식별자 orderId로 ToMany 관계인 OrderItem 을 한꺼번에 조회
     * MAP을 사용해서 매칭 성능 향상(O(1))
    
     List<OrderItemQueryDto> orderItems = findOrderItemMap(toOrderIds(result)); -> List를 in이 :orderIds를 가리켜서 List형식으로 가져와 최적화 시킴
    
      Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
      collect의 Collectors.groupingBy로 Map을 만듬 이건 자바8을 좀 더 공부해서 확인해볼것.

--- 실전스프링부트와JPA활용2-API개발과성능최적화/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ 실전스프링부트와JPA활용2-API개발과성능최적화/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -121,6 +121,10 @@ public class OrderApiController {
        return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5(){
+        return orderQueryRepository.findAllByDto_optimiztion();
+    }
 
 
 
